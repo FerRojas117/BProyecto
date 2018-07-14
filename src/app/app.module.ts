e509667b1eb31a4db36ef002937c30f675793cca
Itzel Farias <fercho117_25halo@hotmail.com>
2018-07-13 23:25:41 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
+// Imports de Material o externos, agregar aqui
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Imports de material
+import {
+  MatToolbarModule
+} from '@angular/material';
+
+/*
+ * AÑADIR COMPONENTES CREADOS A PARTIR DE AQUI
+ * TIP: AGREGAR AL ARREGLO DE "declarations" EN @NgModule, el nombre
+ * de la clase exportada en el componente y se agrega automatico el import
+*/
+
 import { AppComponent } from './app.component';
+// Componente de header para la aplicación
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
+  // Añadir modulos al arreglo de imports
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
